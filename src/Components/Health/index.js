d4e329e349346f479741fac15fc518960b264ac1
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-28 19:36:52 -0800

    adding new component for health section

---
+++ src/Components/Health/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.css'
+function Health() {
+  return (
+    <div className="container rounded">
+      <div className="row">
+        <div className="col">
+      <h1 className="heading">Porque me dedico a la salud?</h1>
+      <hr />
+      <p>Muchos tenemos suños y differentes visiones que queremos ver pasar
+      Yo estoy aqui para que puedes vivir para ver pasar esos sueños</p>
+      </div>
+      <div className="col">
+          <img className="family-image" src="https://c1.wallpaperflare.com/preview/683/135/639/health-insurance-healthcare-insurance-problem.jpg" alt="happy family"/>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Health;
