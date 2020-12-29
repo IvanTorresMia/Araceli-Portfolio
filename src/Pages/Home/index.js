4195b4118426ce0183b6041cf5327c14662e7911
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-28 19:37:24 -0800

    adding new components to home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Hero from '../../Components/Hero'
+import Health from '../../Components/Health'
 
 function Home() {
 
     return (
         <div>
-
+            < Hero/>
+            <Health/>
         </div>
     )
 }
