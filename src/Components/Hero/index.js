3f53583906d85b37a4259fba7e498cb0cc04795c
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-28 19:37:02 -0800

    fixing Hero component

---
+++ src/Components/Hero/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Hero() {
+  return (
+    <div className="jumbotron">
+      <h1>Vivir Saludable</h1>
+      <h3>
+        Llo solo quiero alludarte a aser decisiones para mejorar tu futuro
+      </h3>
+    </div>
+  );
+}
+
+export default Hero;
