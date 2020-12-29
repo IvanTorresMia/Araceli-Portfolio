ef65a6ddf592608c7e4b0c72bc23edc875d428f3
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-28 18:00:22 -0800

    making updates to app.js for router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Router>
+    <div className="App">
      <Navbar />
+     <Route exact path="/" component={Home} />
     </div>
+    </Router>
   );
 }
 
