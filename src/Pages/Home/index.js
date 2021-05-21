cc6decb32c9c6f0c27ce5ab8156f8ca1d97724c3
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-20 17:12:23 -0700

    providing context for cards

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Hero from '../../Components/Hero'
-import Health from '../../Components/Health'
-import Cards from '../../Components/Cards'
+import React from "react";
+import Hero from "../../Components/Hero";
+import AboutMe from "../../Components/AboutMe";
+import Products from "../../Components/Products";
+import CardData from "../../CardData";
+import CardContext from "../../context";
 
 function Home() {
-
-    return (
-        <div>
-            < Hero/>
-            <Health/>
-            <Cards />
-        </div>
-    )
+  return (
+    <div>
+      <Hero />
+      <AboutMe />
+      <CardContext.Provider value={CardData}>
+        <Products />
+      </CardContext.Provider>
+    </div>
+  );
 }
 export default Home;
