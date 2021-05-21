a1f3a55d51d9bc9f2bca2286f4e1ab59b3e2c972
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-20 17:10:12 -0700

    refactoring for cleaner code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import "./App.css";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-     <Navbar />
-     <Route exact path="/" component={Home} />
-    </div>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Home} />
+      </div>
     </Router>
   );
 }
