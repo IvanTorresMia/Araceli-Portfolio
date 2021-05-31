68237478650f151c1d4f43538b18018820a1d107
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-31 12:37:45 -0700

    testimonial data

---
+++ src/data/testiData/index.js
@@ -0,0 +1,26 @@
+import person1 from '../../assets/person1.jpg';
+import person2 from '../../assets/person2.jpg';
+import person3 from '../../assets/person3.jpg';
+
+
+
+const testiData = [
+    {
+        img: person1,
+        name: "Leslie Crandall",
+        testimony: "I struggled with things like anxiety and other stuff now I feel great"
+    },
+    {
+        img: person2,
+        name: "Sarah Smith",
+        testimony: "I struggled with things like anxiety and other stuff now I feel great"
+    },
+    {
+        img: person3,
+        name: "Jake Gabriel",
+        testimony: "I struggled with things like anxiety and other stuff now I feel great"
+    }
+
+];
+
+export default testiData;
