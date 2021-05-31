00cf9aeb73b44dce2ae68a93094862b468862027
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-30 21:44:06 -0700

    using vanilla js to fade components in

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,13 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import CardContext from "../../context";
 import Cards from "./Cards";
 
 const Products = () => {
   const context = useContext(CardContext);
+
+useEffect(() => {
+  const buttons = document.querySelectorAll(".cards-container")
+ window.addEventListener("scroll", () => {
+    buttons.forEach((button) => {
+      let top = button.getBoundingClientRect().top
+
+      if (top < window.innerHeight - 50) {
+        button.classList.add("fade-in-up")
+      }
+    })
+ })
+}, [])
+
   return (
     <div className="container products-container">
         <h2>Ejemplos de Productos</h2>
-      <div className="row card-container">
+      <div className="row cards-container">
         {context.map((data, i) => (
           <Cards
             img={data.img}
