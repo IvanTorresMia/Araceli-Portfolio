8d8a10b5c90b043b13f364ec54663b774f3ffe9e
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-31 09:37:43 -0700

    created about me header

--- src/Components/AboutMeHeader/index.js
+++ src/Components/AboutMeHeader/index.js
@@ -4,8 +4,12 @@ import React from 'react';
 const AboutMeHeader = () => {
 
     return (
-        <div>
-
+        <div className="about-me-header">
+           <h1> Siempre eh sido motivada</h1>
+           <h3>
+               Desde pequeña e batallado con ansiedad y estress,
+                <span className="span1"> estos productos</span> <span className="span2">alludan!</span>
+           </h3>
         </div>
     )
 }
