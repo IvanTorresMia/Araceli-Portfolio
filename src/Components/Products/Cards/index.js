832d920ae3ceba75185036f3fece2452b21adec6
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-30 21:44:24 -0700

    redesigning jsx

--- src/Components/Products/Cards/index.js
+++ src/Components/Products/Cards/index.js
@@ -2,20 +2,16 @@ import React from "react";
 
 const Cards = ({ img, title, description, name }) => {
   return (
-    <div className="product-cards col">
-      <div className="row">
+    <div className="product-cards fade-in">
+      <div className="img-con">
         <img src={img} alt={name} />
       </div>
-      <div className="row card-info">
-        <div className="row">
-          <h3>{title}</h3>
-        </div>
-        <div className="row">
-          <p>{description}</p>
-        </div>
+      <div className="card-info">
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
-      <div className="row Card-footer">
-        <span>{name}</span>
+      <div className="Card-footer">
+        <p>{name}</p>
       </div>
     </div>
   );
