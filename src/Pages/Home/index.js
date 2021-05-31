909721dd8c9619ababe37ea1b332f66eac289700
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-30 21:41:33 -0700

    taking about me off to make its own route

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Hero from "../../Components/Hero";
-import AboutMe from "../../Components/AboutMe";
 import Products from "../../Components/Products";
 import CardData from "../../CardData";
 import CardContext from "../../context";
-import Testimonials from "../../Components/Testimonials"
+import Testimonials from "../../Components/Testimonials";
 
 function Home() {
   return (
     <div>
       <Hero />
-      <AboutMe />
       <CardContext.Provider value={CardData}>
         <Products />
       </CardContext.Provider>
