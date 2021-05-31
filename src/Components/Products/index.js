c9140da9be6571366350f7695b9243828d53aba2
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-31 12:36:48 -0700

    Products algo for animations

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -5,32 +5,34 @@ import Cards from "./Cards";
 const Products = () => {
   const context = useContext(CardContext);
 
-useEffect(() => {
-  const buttons = document.querySelectorAll(".cards-container")
- window.addEventListener("scroll", () => {
-    buttons.forEach((button) => {
-      let top = button.getBoundingClientRect().top
+  useEffect(() => {
+    const buttons = document.querySelectorAll(".cards-container");
+    window.addEventListener("scroll", () => {
+      buttons.forEach((button) => {
+        let top = button.getBoundingClientRect().top;
 
-      if (top < window.innerHeight - 50) {
-        button.classList.add("fade-in-up")
-      }
-    })
- })
-}, [])
+        if (top < window.innerHeight - 50) {
+          button.classList.add("fade-in-up");
+        }
+      });
+    });
+  }, []);
 
   return (
-    <div className="container products-container">
+    <div className="card-background">
+      <div className="container products-container">
         <h2>Ejemplos de Productos</h2>
-      <div className="row cards-container">
-        {context.map((data, i) => (
-          <Cards
-            img={data.img}
-            description={data.description}
-            name={data.name}
-            title={data.title}
-            key={i}
-          />
-        ))}
+        <div className="row cards-container">
+          {context.map((data, i) => (
+            <Cards
+              img={data.img}
+              description={data.description}
+              name={data.name}
+              title={data.title}
+              key={i}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
