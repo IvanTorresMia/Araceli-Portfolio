31300d968f245def64997eff648eb86fcae10b9e
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-30 21:42:26 -0700

    cleaning testimonials for redesign

--- src/Components/Testimonials/index.js
+++ src/Components/Testimonials/index.js
@@ -1,95 +1,10 @@
 import React from "react";
-import blankImage from "../../assets/blank-image.jpg";
 
 const Testimonials = () => {
   return (
-    <div classNameName="testimonials">
-      <div
-        id="carouselExampleCaptions"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <ol class="carousel-indicators">
-          <li
-            data-target="#carouselExampleCaptions"
-            data-slide-to="0"
-            class="active"
-          ></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={blankImage} class="d-block w-100" alt="blank image" />
-            <div class="carousel-caption d-none d-md-block">
-              <h3>First slide label</h3>
-              <p>
-                {" "}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel sint commodi repudiandae
-                consequuntur voluptatum laborum numquam blanditiis harum
-                quisquam eius sed odit fugiat iusto fuga praesentium optio,
-                eaque rerum! Provident similique accusantium nemo autem.
-                Veritatis obcaecati tenetur iure eius earum ut molestias
-                architecto voluptate aliquam nihil, eveniet aliquid culpa
-                officia aut!
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Second slide label</h3>
-              <p>
-                {" "}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel sint commodi repudiandae
-                consequuntur voluptatum laborum numquam blanditiis harum
-                quisquam eius sed odit fugiat iusto fuga praesentium optio,
-                eaque rerum! Provident similique accusantium nemo autem.
-                Veritatis obcaecati tenetur iure eius earum ut molestias
-                architecto voluptate aliquam nihil, eveniet aliquid culpa
-                officia aut!
-              </p>
-            </div>
-            <img src={blankImage} class="d-block w-100" alt="blank image" />
-          </div>
-          <div class="carousel-item">
-            <img src={blankImage} class="d-block w-100" alt="blank image" />
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Third slide label</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-                mollitia, molestiae quas vel sint commodi repudiandae
-                consequuntur voluptatum laborum numquam blanditiis harum
-                quisquam eius sed odit fugiat iusto fuga praesentium optio,
-                eaque rerum! Provident similique accusantium nemo autem.
-                Veritatis obcaecati tenetur iure eius earum ut molestias
-                architecto voluptate aliquam nihil, eveniet aliquid culpa
-                officia aut!
-              </p>
-            </div>
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleCaptions"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleCaptions"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
+<div className="testimonials">
+
+</div>
   );
 };
 
