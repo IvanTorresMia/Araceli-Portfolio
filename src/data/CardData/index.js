050926503afa2d30100c3fd0d8fdb70070efc05e
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-31 12:37:34 -0700

    Card data moved

---
+++ src/data/CardData/index.js
@@ -0,0 +1,45 @@
+import cbdoil from '../../assets/cbdoil.jpg';
+import painlessoil from '../../assets/painlessoil.jpg';
+import producto1 from '../../assets/producto-1.jpg';
+
+
+const CardData = [
+    {
+        img: cbdoil,
+        title: "CBD OIL 5000",
+        name: "ETERNAL",
+        description: "alluda con blah blah blah"
+    },
+    {
+        img: painlessoil,
+        title: "PAINLESS OIL",
+        name: "ETERNAL",
+        description: "alluda con blah blah blah"
+    },
+    {
+        img: producto1,
+        title: "CBD OIL 7000",
+        name: "ETERNAL",
+        description: "alluda con blah blah blah"
+    },
+    {
+        img: cbdoil,
+        title: "CBD OIL 5000",
+        name: "ETERNAL",
+        description: "alluda con blah blah blah"
+    },
+    {
+        img: painlessoil,
+        title: "PAINLESS OIL",
+        name: "ETERNAL",
+        description: "alluda con blah blah blah"
+    },
+    {
+        img: producto1,
+        title: "CBD OIL 7000",
+        name: "ETERNAL",
+        description: "alluda con blah blah blah"
+    }
+]
+
+export default CardData;
