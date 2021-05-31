06802d303d0de5e7fa623209c210c888aa576d27
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-31 12:37:08 -0700

    jsx for testimonials

--- src/Components/Testimonials/index.js
+++ src/Components/Testimonials/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Cards from './cards';
+import testiData from '../../data/testiData';
 
 const Testimonials = () => {
   return (
 <div className="testimonials">
+<h2>Lo Que Dise la Gente</h2>
+    <div className="testi-card-con">
 
+    {testiData.map((data, i) => (
+       <Cards img={data.img} name={data.name} testimony={data.testimony} />
+   ))}
+    </div>
+ 
 </div>
   );
 };
