f49bea973410880f357280eb4fc80804aa6cdd40
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-31 09:37:54 -0700

    created about me hero

---
+++ src/Components/AboutMeHero/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutMeHero = () => {
+  return (
+    <div className="about-me-hero">
+      <h1>"
+“It is health that is the real wealth, and not pieces of gold and silver”</h1>
+      <h4>Mahatma Gandhi</h4>
+    </div>
+  );
+};
+
+export default AboutMeHero;
